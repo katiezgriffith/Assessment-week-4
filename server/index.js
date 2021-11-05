406356e6491bee2f0f3ffa76f3defca7316c1b51
Katie Griffith <angel_9586@hotmail.com>
2021-11-05 17:55:17 -0500

    added fortune button

--- server/index.js
+++ server/index.js
@@ -22,4 +22,23 @@ app.get("/api/compliment", (req, res) => {
   
 });
 
+
+
+app.get("/api/fortune", (req, res) => {
+  const fortunes =[" A fresh start will put you on your way.",
+  "A good time to finish up old tasks." ,
+  "A smile is your personal welcome mat.",
+  "Adventure can be real happiness.",
+  "New ideas could be profitable.",
+
+  ];
+
+ 
+  let randomIndex = Math.floor(Math.random() * fortunes.length);
+  let randomFortune = fortunes[randomIndex];
+
+  res.status(200).send(randomFortune);
+  
+});
+
 app.listen(4000, () => console.log("Server running on 4000"));
